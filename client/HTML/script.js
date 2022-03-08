b4ae052a6064ba6b24d6f982ec19b97ea518ae51
Harry moore <harrywmoore@hotmail.co.uk>
2022-03-08 18:07:38 +0000

    category search working, working on title search

--- client/HTML/script.js
+++ client/HTML/script.js
@@ -1,15 +1,15 @@
 
 let confessionContainer = document.querySelector(".row");
+let AllCats = document.querySelectorAll('h5');
+let Alltitles = document.querySelectorAll('h3');
 let selectCategory = document.querySelector('#searchCategory');
 let button = document.querySelector('#searchConfession');
 let searchTerm = document.querySelector('#searchBar');
-let category = document.querySelector('h5');
-let AllCats = document.querySelectorAll('h5');
+let category = document.querySelector('h5')
 let Col = document.querySelector('COL-6');
 
 
-let AllCats = document.querySelectorAll('h4');
-let category = document.querySelector('h4');
+
 
 
 
@@ -17,20 +17,29 @@ function searchByCategoryOrTitle(){
   if (selectCategory.value === 'Category' && searchTerm.value === ''){
     alert('No search input or category found')
   } else {
-    if(searchTerm.value === '' && selectCategory.value !== 'Category'){
-      for(let i = 0; i < AllCats.length; i++){
-        alert(AllCats[i].textContent)
-        // if(AllCats[i].textContent !== selectCategory.value) {
-        //   AllCats[i].parentElement.parentElement.remove()
-        // }
+      for(let i = 0; i < AllCats.length; i++){  
+        if(AllCats[i].textContent !== selectCategory.value) {
+           AllCats[i].parentElement.parentElement.remove()
+           
+        }
+      }
+      if(searchTerm.value !== '' && selectCategory.value === 'Category'){
+        for(let j = 0; j < Alltitles.length; j++){  
+          if(Alltitles[j].textContent !== searchTerm.value) {
+             Alltitles[j].parentElement.parentElement.remove()
+             
+          }
+        }
+
       }
 
      
-     }
+     
     }
   }
-   
-search.addEventListener('submit', searchByCategoryOrTitle);
+
+fetchCon(); 
+button.addEventListener('click', searchByCategoryOrTitle);
 
   
 
@@ -67,18 +76,18 @@ fetch('http://localhost:3000/confessions').then(function(response) {
             commentPost.id = 'commentContainer'
                 commentPost.append(textComment);
                  ConfessionPost.appendChild(commentPost);
+                 AllCats = document.querySelectorAll('h5');
+                 Alltitles = document.querySelectorAll('h3');
           
 
           }
 
     }
-  }).then(() => button.disabled = false)
+  }).then(() => button.disabled = false )
 
   })
 }
 
-
-fetchCon();
 
 
 
