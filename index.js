43ebcc07825d22b498bb0688fa2c57b2eff40286
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-08 11:27:49 +0000

    added heroku port and if that doesn't work it'll run locally on port 3000

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const server = require('./server');
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 
 server.listen(port, function() {
     console.log('YAY IT WORKS')
 })
-
-// heroku will set a port for you
