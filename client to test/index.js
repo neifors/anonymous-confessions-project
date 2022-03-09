8070404071c95f2cfa203858f802aaf811c26f11
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-09 23:22:47 +0000

    set mode to no cors

--- client to test/index.js
+++ client to test/index.js
@@ -2,48 +2,26 @@ document.getElementById("form").addEventListener("submit", event => {
    event.preventDefault()
 
 
-const newUrl = "https://anonconfess.herokuapp.com/" 
-
-let postData = {
-   title : document.getElementById("title").value,
-   message : document.getElementById("message").value,
-   category : document.getElementById("category").value,
-   comments : [], // default value. As new confession still has no comments
-   reactions : { "like":  0, "love": 0, "hate": 0}, // default value. As new confession still has no reactions
-   gif : document.getElementById("gif").value === undefined ? "" : document.getElementById("gif").value
-}
+//const newUrl = "https://anonconfess.herokuapp.com/" 
 
 
-fetch("https://anonconfess.herokuapp.com/confessions/post", {
-   method : 'POST',
-   body: JSON.stringify(postData),
-   headers : {
-      "Content-Type": "application/json; charset=UTF-8"
-   }
-}) .then( response => {
-   return response.json()
-}) .then( data => {
-   console.log(data)
-})
-
-
-   // fetch("http://localhost:3000/confessions/post", {
-   //    method : 'POST',
-   //    body: JSON.stringify({
-   //       title : document.getElementById("title").value,
-   //       message : document.getElementById("message").value,
-   //       category : document.getElementById("category").value,
-   //       comments : [], // default value. As new confession still has no comments
-   //       reactions : { "like":  0, "love": 0, "hate": 0}, // default value. As new confession still has no reactions
-   //       gif : document.getElementById("gif").value === undefined ? "" : document.getElementById("gif").value
-   //    }),
-   //    headers : {
-   //       "Content-Type": "application/json; charset=UTF-8"
-   //    }
-   // }) .then( response => {
-   //    return response.json()
-   // }) .then( data => {
-   //    console.log(data)
-   // })
+   fetch("http://localhost:3000/confessions/post", {mode: no-cors}, {
+      method : 'POST',
+      body: JSON.stringify({
+         title : document.getElementById("title").value,
+         message : document.getElementById("message").value,
+         category : document.getElementById("category").value,
+         comments : [], // default value. As new confession still has no comments
+         reactions : { "like":  0, "love": 0, "hate": 0}, // default value. As new confession still has no reactions
+         gif : document.getElementById("gif").value === undefined ? "" : document.getElementById("gif").value
+      }),
+      headers : {
+         "Content-Type": "application/json; charset=UTF-8"
+      }
+   }) .then( response => {
+      return response.json()
+   }) .then( data => {
+      console.log(data)
+   })
 
 })
