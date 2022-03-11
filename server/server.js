2a15e2545e04d5797a42f19a47360ee0313b75ff
Hai Anh Nguyen <haianhnguyen@Hais-MacBook-Air.local>
2022-03-11 00:31:16 +0000

    router.use(bodyParser) changed to app.use(bodyParser) and placed in this file- went from 204 status to 200

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const app = express()
 const router = require('./controller/controller.js')
 const cors = require('cors');
 const path = require('path');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
@@ -11,7 +12,6 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json());
 app.use(
    cors({
       origin: "*",
@@ -21,6 +21,13 @@ app.use(
 
 app.use(express.json())
 app.use('/confessions', router)
+
+// const dataFile = require('./data/data.json');
+
+// //sends data.json - lets you see what the post method has added - deleted route but not actual file- data.json
+// app.get('/datafile', (req, res) => {
+//     res.json(dataFile);
+// });
 
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, '/homepage/index.html'))
